dee2c3af0341e744036fdee568f65e9a3594dcb5
jitndrasingh <j.s.r.p.vingarla@gmail.com>
2020-06-29 18:53:39 +0530

    Simple Java code

---
+++ JavaWebTest/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello World Welcome to Java");
+         
+         for (int i=0; i<5; i++){
+        	 System.out.println(i);
+         }
+	}
+
+}
